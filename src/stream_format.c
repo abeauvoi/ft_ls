9cf181230e14279d904ff231785161477fc73193
Alexandre BEAUVOIS <abeauvoi@student.42.fr>
2018-06-14 00:55:06 +0200

    -m in progress; patch buffer overflow in short_format.c

--- src/stream_format.c
+++ src/stream_format.c
@@ -1,28 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   short_format.c                                     :+:      :+:    :+:   */
+/*   stream_format.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abeauvoi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/04/09 06:58:04 by abeauvoi          #+#    #+#             */
-/*   Updated: 2018/06/13 22:21:06 by abeauvoi         ###   ########.fr       */
+/*   Created: 2018/06/13 22:19:46 by abeauvoi          #+#    #+#             */
+/*   Updated: 2018/06/13 22:48:37 by abeauvoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void	short_format(t_fileinfo *entry, t_ls *info)
+void	stream_format(t_fileinfo *entry, t_ls *info)
 {
-	char	buf[255 + 1 + 1];
-
-	ft_strncpy(buf, entry->name, MIN(255, entry->namlen));
-	buf[entry->namlen] = '\n';
-	buf[entry->namlen + 1] = 0;
-	if (entry->ctab_index != NO_COLOR)
-		strtobuf(info, info->ctab[entry->ctab_index],
-				info->len_color[entry->ctab_index]);
-	strtobuf(info, buf, entry->namlen + 1);
-	if (entry->ctab_index != NO_COLOR)
-		strtobuf(info, RESET_COLOR, 3);
 }
